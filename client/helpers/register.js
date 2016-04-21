20b455c9b36650fb87e12e5a64431b65c8c9847a
Keivan Shahida <keivanshahida@yahoo.com>
2016-04-20 21:44:46 -0400

    Body Class Package added. Preliminary Schedule tempate and route added.

--- client/helpers/register.js
+++ client/helpers/register.js
@@ -8,26 +8,34 @@ Template.register.events({
         var password = $('[name=password]').val();
         Accounts.createUser({
             email: email,
-            password: password,
-            profile: {
-                firstname: firstname,
-                lastname: lastname,
-                school: school,
-            }
-
+            password: password
         }, function(error){
             if(error){
                 console.log(error.reason); // Output error if registration fails
             } 
             else {
             Router.go("search"); // Redirect user if registration succeeds
-            }
+        }
+    });
+
+        Accounts.onCreateUser((options, user) => {
+            var firstname = "John";
+            var lastname = "Smithson";
+
+            user.initials = first_name[0].toUpperCase() + last_name[0].toUpperCase();
+
+            // Don't forget to return the new user object at the end!
+            return user;
         });
     }
 });
 
+
+
+//USE THIS LINK FOR CUSTOM DATA INFO: http://guide.meteor.com/accounts.html#custom-user-data 
+
 // TO ADD USER MANUALLY IN CONSOLE:
-// Accounts.createUser({email: "ks@icloud.com", password: "1234", type: "student", firstname: "Keivan", lastname: "Shahida", grad_year: 2016, school: "Horace Mann School"});
+// Accounts.createUser({email: "skim@icloud.com", password: "1234", profile: {firstname: "Shawn", lastname: "Kim", school: "Horace Mann School", grad_year: 2016}});
 // Accounts.createUser({email: "ks@icloud.com", password: "1234"});
 
 // Student: (
