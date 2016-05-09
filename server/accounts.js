4c029d82ef88c6337138f4d53e7a6fcdd4332476
Keivan Shahida <keivanshahida@yahoo.com>
2016-05-08 20:57:05 -0400

    Search without criteria fixed.

--- server/accounts.js
+++ server/accounts.js
@@ -1,21 +1,26 @@
  Accounts.onCreateUser((options, user) => {
 
- 	user.profile = {country: "USA"};
+ 	user.profile = {
+                firstname: "Shawn", 
+                lastname: "Kim", 
+                school: "Horace Mann School", 
+                grad_year: 2016
+            };
 
- 	var mySchool = "Los Angeles High School";
- 	user.school = mySchool;
+ 	// var mySchool = "Los Angeles High School";
+ 	// user.school = mySchool;
 
- 	var myFirstname = "John";
- 	user.firstname = myFirstname;
+ 	// var myFirstname = "John";
+ 	// user.firstname = myFirstname;
 
- 	var myLastname = "Smith";
- 	user.lastname = myLastname;
+ 	// var myLastname = "Smith";
+ 	// user.lastname = myLastname;
 
- 	var myGradyear = 2016; 
- 	user.gradyear = myGradyear;
+ 	// var myGradyear = 2016; 
+ 	// user.gradyear = myGradyear;
 
- 	user.initials = myFirstname[0].toUpperCase() + myLastname[0].toUpperCase();
- 	console.log("Hello", user);
+ 	// user.initials = myFirstname[0].toUpperCase() + myLastname[0].toUpperCase();
+ 	// console.log("Hello", user);
 
     // Don't forget to return the new user object at the end!
     return user;
