b3bcddf743117dca95133fdb34d053a07c44ee06
Keivan Shahida <keivanshahida@yahoo.com>
2016-05-23 08:57:51 -0400

    Basic, hardcoded course addition added

--- client/helpers/coursepage.js
+++ client/helpers/coursepage.js
@@ -1,7 +1,13 @@
 Template.coursepage.events({
 	'submit form': function(event){
 		event.preventDefault();		
-	}
+	},
+
+	'click #saveChanges': function (event) {
+    // save changes to course block
+    Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.schedule.twelfth.course1.choice1": "0451"}});
+  	}
+
 
 	// 'click btn btn-primary': function(event){
 	// 	event.preventDefault();
